739fa67fc73162bf31099c33b2b00b9fc6526032
tillenotuoma <totuoma27@gmail.com>
2025-04-05 22:42:29 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        url = self.url
+        response = requests.get(url)
+        return response.content
+
         pass
 
     def load_json(self):
+        data = self.get_response_body()
+        converted_data = json.loads(data)
+        return converted_data
         pass
